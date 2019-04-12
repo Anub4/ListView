4b7758e42253710475479757b22d0a023b8375df
Softwarica College Lab <softwarica@softwarica.com>
2019-04-12 08:11:22 +0545

    Complete hashmap

--- app/src/main/java/np/com/softwarica/listview/MainActivity.java
+++ app/src/main/java/np/com/softwarica/listview/MainActivity.java
@@ -2,12 +2,52 @@ package np.com.softwarica.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String countries[] = {
+            "Nepal", " Kathmandu", "Indonesia", "Bali", "Maldives", "Male", "USA", "Washington DC"
+    };
+
+    private Map<String,String> dictionary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lv1 = findViewById(R.id.lv1);
+
+        dictionary = new HashMap<>();
+
+        for (int i=0; i<countries.length;i+=2){
+            dictionary.put(countries[i],countries[i+1]);
+        }
+
+        ArrayAdapter countryAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1,
+                new ArrayList<String>(dictionary.keySet())
+        );
+
+        lv1.setAdapter(countryAdapter);
+
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String country = parent.getItemAtPosition(position).toString();
+                String capital = dictionary.get(country);
+                Toast.makeText(getApplicationContext(),capital.toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
